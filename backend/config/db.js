7dbb491cd0964df35b6037382d310cbf785a63a7
swapnil solanki <ssinghsolanki686@gmail.com>
2023-06-15 13:39:37 +0530

    Database and port number add and create index

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+const connection = mongoose.connect(process.env.db_url)
+
+
+module.exports = {connection}
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const { connection } = require("./config/db");
 require("dotenv").config()
-
+const mongosoe = require("mongoose")
 const app = express()
-
 app.use(express())
 
-app.listen(process.env.port,()=>{
+
+
+app.listen(process.env.port,async ()=>{
+    await connection
     console.log("server is running")
 })
