24efe122079f417e57edfe894467aff8f6d47466
bethimanideep <bethimanideeep@gmail.com>
2023-06-16 00:25:47 +0530

    signuppagecompleted

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
-const connection = mongoose.connect(process.env.db_url)
+const connection = mongoose.connect(process.env.DATABASE)
 
 
 module.exports = {connection}
